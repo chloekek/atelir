5ed4cf4d470dc4594c2edb8937a5d5d69231bd15
Chloé Kekoa <rightfold+ck@gmail.com>
2020-09-19 19:05:57 +0200

    Turn post titles into permalinks

---
+++ software/lib/HasPostUri.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types = 1);
+namespace Atelir;
+
+trait HasPostUri
+{
+    final public
+    function postUri(): string
+    {
+        return '/' . $this->ownerSlug .
+               '/' . $this->projectSlug .
+               '/' . $this->slug;
+    }
+}
--- software/www/index.php
+++ software/www/index.php
@@ -2,49 +2,69 @@
 declare(strict_types = 1);
 require_once __DIR__ . '/../lib/setup.php';
 
+use Atelir\HasPostUri;
 use Atelir\Layout;
 
 final
 class FeaturedPost
 {
+    use HasPostUri;
+
+    public string $ownerSlug;
+    public string $projectSlug;
+    public string $slug;
     public string $title;
     public string $content;
 
     public
-    function __construct(string $title, string $content)
+    function __construct(
+        string $ownerSlug,
+        string $projectSlug,
+        string $slug,
+        string $title,
+        string $content
+    )
     {
+        $this->ownerSlug = $ownerSlug;
+        $this->projectSlug = $projectSlug;
+        $this->slug = $slug;
         $this->title = $title;
         $this->content = $content;
     }
 }
 
 $rows = $postgresql->query('
-    SELECT title, content
+    SELECT owner_slug, project_slug, slug, title, content
     FROM atelir.posts
     WHERE published IS NOT NULL
     ORDER BY published DESC
 ');
 
-$fps = call_user_func(
+$fps = \call_user_func(
     /** @return iterable<int,FeaturedPost> */
     function() use($rows): iterable {
         foreach ($rows as $row) {
             assert($row[0] !== NULL);
             assert($row[1] !== NULL);
-            yield new FeaturedPost($row[0], $row[1]);
+            assert($row[2] !== NULL);
+            assert($row[3] !== NULL);
+            assert($row[4] !== NULL);
+            yield new FeaturedPost($row[0], $row[1], $row[2], $row[3], $row[4]);
         }
     },
 );
 
 Layout::layout('Home', function() use($fps): void {
     foreach ($fps as $fp) {
-        echo '<article class="atelir-post" dir="auto">';
-        echo '<h1>';
-        echo \htmlentities($fp->title);
-        echo '</h1>';
-        echo '<p>';
-        echo \htmlentities($fp->content);
-        echo '</p>';
-        echo '</article>';
+        ?><article class="atelir-post" dir="auto"><?php
+            ?><h1><?php
+                ?><a href="<?= \htmlentities($fp->postUri()) ?>"><?php
+                    ?><?= \htmlentities($fp->title) ?><?php
+                ?></a><?php
+            ?></h1><?php
+            ?><p><?php
+                ?><?= \htmlentities($fp->content) ?><?php
+            ?></p><?php
+        ?></article><?php
     }
 });
